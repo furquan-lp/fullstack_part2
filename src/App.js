64090a31d3cbd3cb6529cf7033c05df0de00cb38
Syed Furquan Ahmad <furquan@pm.me>
2022-03-17 20:18:48 +0530

    2.6: The Phonebook Step1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import Course from './components/Course';
+import { useState } from 'react';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ];
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addName = (event) => {
+    event.preventDefault();
+    const submittedName = { name: newName };
+    setPersons(persons.concat(submittedName))
+    setNewName('');
+  };
+
+  const handleTextChange = (event) => {
+    setNewName(event.target.value);
+  }
+
   return (
-    <>
-      <h1>Web development curriculum</h1>
-      {courses.map((course) => <Course course={course} key={course.id} />)}
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleTextChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
   );
 };
 
