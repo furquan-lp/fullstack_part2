ed24d002336e31c86b52cef14ccf9d9f1af3f33f
Syed Furquan Ahmad <furquan@pm.me>
2022-03-26 22:59:33 +0530

    2.15: Phonebook step7

--- src/App.js
+++ src/App.js
@@ -1,104 +1,54 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Filter } from './components/Filter';
 
-const api_key = process.env.REACT_APP_API_KEY;
+const SearchField = ({ searchText, handleSearch }) =>
+  <>filter shown with a<input value={searchText} onChange={handleSearch} /></>;
 
-const SearchField = ({ text, searchText, handleSearch }) =>
-  <>{text}<input value={searchText} onChange={handleSearch} /></>;
-
-const Country = ({ country, oWeather }) =>
-  <><h2>{country.name.official}</h2>
-    <p>capital {country.capital}</p>
-    <p>area {country.area}</p>
-    <b>languages</b>
-    <ul>
-      {Object.keys(country.languages)
-        .map(language => <li key={language}>{country.languages[language]}</li>)}
-    </ul>
-    <img src={country.flags.png} alt={'Flag of ' + country.name.common} />
-    {oWeather.length !== 0 ?
-      <Weather capital={country.capital[0]} oWeather={oWeather} />
-      : null}
-  </>;
-
-const Weather = ({ capital, oWeather }) =>
-  <><h2>Weather in {capital}</h2>
-    <p>temperature {oWeather.current.temp} Celsius</p>
-    <WeatherIcon weather={oWeather.current.weather[0]} />
-    <p>wind {oWeather.current.wind_speed} m/s</p>
-  </>;
-
-
-const WeatherIcon = ({ weather }) =>
-  <><img src=
-    {'http://openweathermap.org/img/wn/' + weather.icon + '@2x.png'}
-    alt={'Weather icon for ' + weather.description} /></>;
-
-const Countries = ({ countriesToShow, onShowButtonClick, showIndex, oWeather }) => {
-  if (countriesToShow.length === 0) return null;
-  const names = countriesToShow.map(country => country.name);
-  const mapped = names.map((name, i) =>
-    <p key={name.official}>
-      {name.common}<button id={i} onClick={onShowButtonClick}>show</button>
-    </p>);
-  return (
-    <>
-      {mapped.length <= 10 ? mapped : <p>Too many matches</p>}
-      {showIndex >= 0 ?
-        <Country country={countriesToShow[showIndex]} oWeather={oWeather} />
-        : null}
-    </>);
-};
+const Form = ({ onSubmit, newPerson, handleName, handleNum }) =>
+  <form onSubmit={onSubmit}>
+    <div>name: <input value={newPerson.name} onChange={handleName} /></div>
+    <div>number: <input value={newPerson.number} onChange={handleNum} /></div>
+    <div><button type="submit">add</button></div>
+  </form>;
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
+  const [persons, setPersons] = useState([{ name: '', number: '' }]);
+  const [newPerson, setNewPerson] = useState({ name: '', number: '' });
   const [searchText, setSearch] = useState('');
-  const [showCountry, setShowCountry] = useState(-1);
-  const [latlng, setLatlng] = useState([0, 0]);
-  const [oWeather, setOWeather] = useState([]);
 
-  const restCountriesHook = () => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => setCountries(response.data));
-  };
-  const oWeatherHook = () => {
-    const getter = 'https://api.openweathermap.org/data/2.5/onecall?lat='
-      + latlng[0] + '&lon=' + latlng[1] +
-      '&exclude=minutely,hourly,daily&appid=' + api_key;
-    if (!(latlng[0] === 0 && latlng[1] === 0)) {
-      axios
-        .get(getter)
-        .then(response => setOWeather(response.data));
+  const url = 'http://localhost:3001/persons';
+  useEffect(() => axios.get(url)
+    .then(response => setPersons(response.data)), []);
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (persons.findIndex(p =>
+      p.name === newPerson.name && p.number === newPerson.number) === -1) {
+      axios.post(url, newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewPerson({ name: '', number: '' });
+        });
+    } else {
+      alert(`${newPerson.name} already exists.`);
     }
   };
-  useEffect(restCountriesHook, []);
-  useEffect(oWeatherHook, latlng);
 
-  const handleSearch = (event) => {
-    setSearch(event.target.value);
-    setShowCountry(-1);
-  };
-  const handleShowButton = (event) => {
-    if (event.target.id >= 0) {
-      const currentLatlng = countriesToShow[event.target.id].capitalInfo.latlng;
-      if (currentLatlng[0] !== latlng[0] || currentLatlng[1] !== latlng[1])
-        setLatlng(currentLatlng);
-    }
-    setShowCountry(showCountry !== event.target.id ? event.target.id : -1);
-  };
-
-  const countriesToShow = searchText === '' ? [] : countries.filter(country =>
-    country.name.common.toLowerCase().includes(searchText.toLowerCase()));
+  const handleNameChange = (event) =>
+    setNewPerson({ name: event.target.value, number: newPerson.number });
+  const handleNumChange = (event) =>
+    setNewPerson({ name: newPerson.name, number: event.target.value });
+  const handleSearch = (event) => setSearch(event.target.value);
 
   return (
     <div>
-      <h2>Countries</h2>
-      <SearchField text='find countries' searchText={searchText} handleSearch={handleSearch} />
-      <Countries countriesToShow={countriesToShow}
-        onShowButtonClick={handleShowButton}
-        showIndex={showCountry}
-        oWeather={oWeather} />
+      <h2>Phonebook</h2>
+      <SearchField searchText={searchText} handleSearch={handleSearch} />
+      <h3>add a new</h3>
+      <Form onSubmit={addName} newPerson={newPerson} handleName={handleNameChange} handleNum={handleNumChange} />
+      <h2>Numbers</h2>
+      <Filter persons={persons} searchText={searchText} setSearch={setSearch} />
     </div>
   );
 };
